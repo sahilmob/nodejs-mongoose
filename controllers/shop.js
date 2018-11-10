43eef4f98a8920d1c8752f2a1d8e9b548603b334
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-10 16:02:16 +0200

    add logic to render cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -43,12 +43,13 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
 	req.user
-		.getCart()
-		.then(products => {
+		.populate("cart.items.productId")
+		.execPopulate()
+		.then(user => {
 			res.render("shop/cart", {
 				path: "/cart",
 				pageTitle: "Your Cart",
-				products: products
+				products: user.cart.items
 			});
 		})
 		.catch(err => console.log(err));
